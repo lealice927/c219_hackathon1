d53db14a89c02bac69d2fca944bf53e51666a404
Christopher Gormley <Chrisg913@gmail.com>
2019-03-07 10:35:51 -0800

    added roll button jquery, single roll, and dice container

--- dice.js
+++ dice.js
@@ -1,17 +1,27 @@
-
-
+class DiceContainer{
+    constructor(){
+       // this.selectedDice = [];
+        this.Dice = [];
+       // this.rollsRemaining = 3;
+    }
+}
+//use push method to send dice to selected dice
+// class SelectedDice{
+//     constructor(){
+//        return this.selectedDice.add( randomFace );
+//     }
+// }
 class Dice{
     constructor(){
         this.availableFaces = [1,2,3,'Fist','Bolt','Heart'];
         this.domElement = null;
+       // this.dice.push( selectedDice );
     }
     roll(){
         var randomFaceIndex = Math.floor(Math.random() * this.availableFaces.length);
         var randomFace = this.availableFaces[randomFaceIndex];
         this.domElement.text( randomFace );
-        
-
-   
+       
     }
     render(){
         this.domElement = $("<div>")
@@ -20,3 +30,10 @@ class Dice{
         return this.domElement;
     }
 }
+
+// class RollsRemaining{
+//     constructor(){
+
+//     }
+// }
+
--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-$(document).ready(startApp);
+
 
 class KingOfTokyo {
     constructor(){
